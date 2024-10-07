4d1590dd5324f33c2a215432a8366303a1a2893b
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-10-08 00:20:33 +0900

    feat : components path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Navbar, Hero, Highlights } from "./components/index";
+
 const App = () => {
-  return <main className="bg-black">Navbar hero highlights</main>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
