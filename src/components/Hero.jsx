e631560f7d6e56e7e559d812cc69eb3bbba12c7f
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-10-11 22:06:05 +0900

    feat : iphone vider set

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,33 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerHeight < 760 ? smallHeroVideo : heroVideo
+  );
+
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  }, []);
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
