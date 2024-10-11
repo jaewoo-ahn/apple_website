9db239f03a5d8111db9b1c2b08f4bd5255f34d17
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-10-11 21:05:31 +0900

    feat : basic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navArray = ["Phones", "Macbooks", "Tablets"];
+
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple" width={14} height={18} />
+        <div>
+          {navArray.map((res, idx) => {
+            return <div key={idx}>{res}</div>;
+          })}
+        </div>
+        <div>
+          <img src={searchImg} alt="search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
