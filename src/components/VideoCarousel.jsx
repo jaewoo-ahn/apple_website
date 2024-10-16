cae611c74d2b364520de601fb996b01a7fcc4245
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-10-16 21:31:40 +0900

    test : video carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,5 +1,17 @@
+import { hightlightsSlides } from "../constants";
+
 const VideoCarousel = () => {
-  return <div>VideoCarousel</div>;
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, idx) => {
+          <div key={list.id} id="slider">
+            <div className="video-carousel_container">TEST</div>
+          </div>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default VideoCarousel;
